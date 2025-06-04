e9309806ebd02085b6321bcd1dc7b5d3051a00f5
unknown <jefersonosimar13@gmail.com>
2025-06-03 21:39:58 -0300

    inicio da aula 01 JS

--- JS/JS01-Introdução/main.js
+++ JS/JS01-Introdução/main.js
@@ -1,1 +1,34 @@
-fkdfdjkfff
+var idade
+//JavaScript
+console.log("Hello World") //apresentar uma mensagem
+console.clear() //limpar mensagem 
+
+//Variaveis
+
+var numero1 = 192 //inicializar uma variavel
+console.log(numero1)
+
+var num1 = 4
+var num2 = 5
+console.log(num1 + num2)
+
+var resultado = num1 * num2
+console.log(resultado)
+
+idade = 89 // inicializar a variavel em outra parte do console
+console.log(idade)
+
+//Textos - Strings
+
+var nome = "Lorem"
+var nome2 = "Tardis"
+
+console.log(nome)
+console.log(nome2)
+
+
+VAR // Flexivel e com menos regras (posso alterar o valor inicial de u,a var e recriar a mesma em outras partes do codigo)
+
+LET // Você não pode recriar a mesma em outras partes do cidugo com o mesmo noem, mas pode chamar e alterar o valro inicial 
+
+Const // Você não pode recruar a mesma em outaras partes do codigo enão pode alterar o vallor inicial dela garantindo a confiança do dado que está sendo utilizado no decorrer do seu codigo 
