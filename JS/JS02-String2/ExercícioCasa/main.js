51a20e780602a8157f0ce7f40e8429548bd1afe4
Jeferson Osimar <jefersonosimar13@gmail.com>
2025-06-15 18:04:34 -0300

    Atualizar o main.js
    envio final do exercício casa pelo celular

--- JS/JS02-String2/ExercícioCasa/main.js
+++ JS/JS02-String2/ExercícioCasa/main.js
@@ -1,1 +1,8 @@
-console.log
+const texto = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+
+console.log("Caractere na posição 19: ", texto.charAt(19))
+
+console.log("Tamanho da string: ", texto.length)
+
+console.log("Texto em maiúsculo: ", texto.toUpperCase())
