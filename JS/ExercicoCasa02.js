ae6623350bbb2b4a958a5320a526e6bd6033681b
Jeferson Osimar <jefersonosimar13@gmail.com>
2025-06-15 16:54:37 -0300

    Criar ExercicoCasa02.js

---
+++ JS/ExercicoCasa02.js
@@ -0,0 +1,8 @@
+const texto = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+
+console.log("Caractere na posição 19: ", texto.charAt(19))
+
+console.log("Tamanho da string: ", texto.length)
+
+console.log("Texto em maiúsculo: ", texto.toUpperCase())
