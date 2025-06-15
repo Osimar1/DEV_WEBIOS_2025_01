f160bcf45c69e56d43b0c648ca5615dc41a8fd71
Jeferson Osimar <jefersonosimar13@gmail.com>
2025-06-15 18:10:01 -0300

    Atualizar o main.js
    Envio do exército Casa pelo Celular

--- JS/JS04-LacosdeRepeticoes/ExercicioCasa/main.js
+++ JS/JS04-LacosdeRepeticoes/ExercicioCasa/main.js
@@ -1,1 +1,10 @@
-console.log
+
+
+if (tabuada === 2 || tabuada === 3) {
+  console.log("Tabuada do " + tabuada + ":");
+  for (var i = 1; i <= 10; i++) {
+    console.log(tabuada + " x " + i + " = " + (tabuada * i));
+  }
+} else {
+  console.log("Este programa só imprime a tabuada do 2 ou do 3.");
+}
